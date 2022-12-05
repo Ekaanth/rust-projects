9f59c0168fd165cd2ddabcd6cbd7688072fda588
Abhishek <akanth1994@gmail.com>
2022-12-06 00:51:35 +0530

    web scraper initial commit

---
+++ scrape-website/src/main.rs
@@ -0,0 +1,14 @@
+use chrono;
+use regex::Regex;
+use request::StatusCode;
+use scraper::{Html, Selector};
+use std::fs::File;
+use std::io::Write;
+
+mod models;
+mod utils;
+
+#[tokio::main]
+async fn main(){
+    
+}
