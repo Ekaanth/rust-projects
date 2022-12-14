8f2a988f0401067eadf14c2122e641ecada485aa
Abhishek <akanth1994@gmail.com>
2022-12-14 23:51:41 +0530

    dependency added

--- FirebaseCrud/src/main.rs
+++ FirebaseCrud/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use firebase_rs::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct User {
+    name: String,
+    age: u32,
+    email:String
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Response {
+    name: String
+}
+
+
+#[tokio::main]
+async fn main() {
+    let user = User {
+        name: "Abhishek".to_string(),
+        age: 30,
+        email: "test.email@gmail.com".to_string(),
+    };
+
+
+
+
 }
