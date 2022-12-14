487b708c03ec9b36d7dc84bc4924ab88a8267506
Abhishek <akanth1994@gmail.com>
2022-12-15 01:06:18 +0530

    updated

--- FirebaseCrud/src/main.rs
+++ FirebaseCrud/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use firebase_rs::*;
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +14,46 @@ struct Response {
     name: String
 }
 
+async fn set_user(firebase_client: &Firebase, user: &User) -> Response {
+let firebase = firebase_client.at("users");
+    let _users = firebase.set::<User>(&user).await;
+    return string_to_response(&_users.unwrap().data);
+}
+
+
+async fn get_users(firebase_client: &Firebase) -> HashMap<String, User>{
+    let firebase = firebase_client.at("users");
+    let users = firebase.get::<HashMap<String, User>>().await;
+    println!("{:?}", users);
+    return users.unwrap();
+}
+
+async fn get_user(firebase_client: &Firebase, id: &String) -> User{
+    let firebase = firebase_client.at("users").at(&id);
+    let user = firebase.get::<User>().await;
+    return user.unwrap();
+}
+
+async fn update_user(firebase_client: &Firebase, id: &String, user: &User) -> User {
+    let firebase = firebase_client.at("users").at(&id);
+    let _user = firebase.update::<User>(&user).await;
+    println!("{:?}", _user);
+    return string_to_user(&_user.unwrap().data);
+
+}
+
+async fn delete_user(firebase_client: &Firebase, id: &String) {
+    let firebase = firebase_client.at("user").at(&id);
+    let _result = firebase.delete().await;
+}
+
+fn string_to_response(s: &str) -> Response {
+    serde_json::from_str(s).unwrap()
+}
+
+fn string_to_user(s: &str) -> User {
+    serde_json::from_str(s).unwrap()
+}
 
 #[tokio::main]
 async fn main() {
@@ -22,7 +63,20 @@ async fn main() {
         email: "test.email@gmail.com".to_string(),
     };
 
+    let firebase = Firebase::new("https://simple-crud-rust-default-rtdb.firebaseio.com/").unwrap();
 
+    let response = set_user(&firebase, &user).await;
 
+    let mut user = get_user(&firebase, &response.name).await;
+    println!("{:?}", user);
 
+    let users = get_users(&firebase).await;
+    println!("{:?}", users);
+
+    user.email = "updated.email@gmail.com".to_string();
+
+    let updated_user = update_user(&firebase, &response.name, &user);
+
+    delete_user(&firebase, &response.name).await;
+    println!("User Deleted");
 }
