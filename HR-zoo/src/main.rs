560dcc2d073cef64c1fb0bcf3aecad313b5ac3e3
Abhishek <akanth1994@gmail.com>
2022-12-20 11:59:12 +0530

    HR problem

---
+++ HR-zoo/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+    let mut x = 0;
+    let mut y = 0;
+    let name = &name.to_lowercase();
+    let chars: Vec<char> = name.chars().collect();
+    for (index, element) in chars.iter().enumerate() {
+        if *element == 'z' {
+            x += 1;
+        }else if *element == 'o'{
+            y += 1;
+        }
+
+
+
+        
+    }
+    if x*2 == y {
+        println!("Yes");
+    }else{
+        println!("No");
+    }
+}
