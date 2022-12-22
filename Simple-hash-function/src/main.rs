a3796a5e7b9607ebee1ddbff2d91b890648daf85
Abhishek <akanth1994@gmail.com>
2022-12-22 11:06:27 +0530

    updated with terminal input

--- Simple-hash-function/src/main.rs
+++ Simple-hash-function/src/main.rs
@@ -2,17 +2,15 @@ use std::env;
 use sha256::digest;
 
 fn main() {
-    let input = "hello";
-    let val = digest(input);
-    println!("{}",val);
-
-    let input = "hello".to_string();
-    let val = digest(input);
-    println!("{}",val);
-
-
-    let input = b"hello";
-    let val = digest(input);
-    println!("{}",val);
-
+    let args: Vec<_> = std::env::args().collect();
+    println!("{}", args.len());
+    if args.len() == 1 {
+        println!("Usage: Please enter one text to hash");
+        return;
+    }
+    for i in 0..args.len() {
+        let args_1 = &*args[i];
+        let mut hasher = digest(args_1.to_string());
+        println!("{} => {}",args[i], hasher);
+    }
 }
