cccad8bce0e7ce2b5e20a6b9e09c8a5906347945
Abhishek <akanth1994@gmail.com>
2023-01-11 23:53:02 +0530

    Error handled

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,49 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process; 
+
+struct Config {
+    query : String,
+    filename: String
+}
+
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok((Config { query, filename}))
+}
+}
+
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Search for {}", query);
-    println!("in file {}", filename);
-    let contents = fs::read_to_string(filename)
-                            .expect("Some went wrong reading the file");
-    println!("with text: \n {}", contents);
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Search for {}", config.query);
+    println!("in file {}", config.filename);
+    
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
+
+fn run (config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("with text: \n {}", contents);
+    Ok(())
+}
+
