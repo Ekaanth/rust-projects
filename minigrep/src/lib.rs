76e2713a06c184b37297cea6efac261eb93bcc25
Abhishek <akanth1994@gmail.com>
2023-01-18 10:58:50 +0530

    update

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,14 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
 
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         
         Ok(Config { query, filename, case_sensitive})
@@ -41,14 +47,9 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
 
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut  results = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    content.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
